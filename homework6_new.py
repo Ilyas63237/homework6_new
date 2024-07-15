7be6c5924c4eeae2e441cdaf831fd69783634359
Ilyas63237 <vakhitov.ilyas@bk.ru>
2024-07-15 10:24:15 +0300

    Add files via upload

---
+++ homework6_new.py
@@ -0,0 +1,16 @@
+my_dict = {'Ilyas': 1997, 'Nastya': 1994, 'Andrey': 1995}
+print(my_dict)
+print(my_dict['Ilyas'])
+print(my_dict.get('Dasha', 'Error'))
+my_dict.update({'Dima': 1901, 'Vlad': 2001})
+del my_dict['Nastya']
+print(my_dict)
+my_dict.pop('Ilyas')
+print(my_dict)
+
+my_set = {1, 2, 2, 3, 1, 3, 2, 4, 'a', 'a', 'b', (2, 5, 5.5)}
+print(my_set)
+print(my_set.remove('a'))
+print(my_set.add(20,))
+print(my_set.add(15,))
+print(my_set)
